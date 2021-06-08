498d10bfb9c1957c29dca609f738dee34876c761
gabriellaszigeti <gbszgt@gmail.com>
2021-06-08 12:00:48 +0200

    Feat: get and set methods for coordinates

--- src/main/java/com/branchhunters/polishdraughts/Coordinates.java
+++ src/main/java/com/branchhunters/polishdraughts/Coordinates.java
@@ -1,4 +1,25 @@
 package com.branchhunters.polishdraughts;
 
 public class Coordinates {
+
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
+
+
 }
