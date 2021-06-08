adab0e7a10b0eb3205adba7ed844dd68016b33c3
Sairieth <henrik.riki.marcz@gmail.com>
2021-06-08 15:50:38 +0200

    feat:  modify setPawns line 20,35 statements

--- src/main/java/com/branchhunters/polishdraughts/Board.java
+++ src/main/java/com/branchhunters/polishdraughts/Board.java
@@ -17,30 +17,30 @@ public class Board {
 
         for (int row = 0; row < board.length; row++) {
             if (blackPieces >= 0) {
-                if (row + 1 % 2 == 0) {
-                    for (int i = 0; i < row; i += 2) {
+                if ((row + 1) % 2 == 0) {
+                    for (int i = 0; i < board[row].length; i += 2) {
                         board[row][i] = new Pawn("black", row, i);
-                        blackPieces = -1;
+                        blackPieces--;
                     }
                 } else {
-                    for (int i = 1; i < row; i += 2) {
+                    for (int i = 1; i < board[row].length; i += 2) {
                         board[row][i] = new Pawn("black", row, i);
-                        blackPieces = -1;
+                        blackPieces--;
                     }
                 }
             }
         }
         for (int row = board.length - 1; row >= 0; row--) {
             if (whitePieces >= 0) {
-                if (row - 1 % 2 == 0) {
-                    for (int i = row; i > 0; i--) {
+                if ((row - 1) % 2 == 0) {
+                    for (int i = board[row].length-1; i > 0; i--) {
                         board[row][i] = new Pawn("white", row, i);
-                        whitePieces = -1;
+                        whitePieces--;
                     }
                 } else {
-                    for (int i = row; i > 0; i--) {
+                    for (int i = board[row].length-2; i > 0; i--) {
                         board[row][i] = new Pawn("white", row, i);
-                        whitePieces = -1;
+                        whitePieces--;
                     }
                 }
             }
