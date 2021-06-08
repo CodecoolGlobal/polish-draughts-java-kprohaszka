5b9b6b5133bf1610bbd3a781de66c8247c4f91e3
kprohaszka <cc.kprohaszka@gmail.com>
2021-06-08 16:22:50 +0200

    Fix board generation loop indexing

--- src/main/java/com/branchhunters/polishdraughts/Board.java
+++ src/main/java/com/branchhunters/polishdraughts/Board.java
@@ -16,33 +16,37 @@ public class Board {
         int whitePieces = n * 2;
 
         for (int row = 0; row < board.length; row++) {
-            if (blackPieces >= 0) {
                 if ((row + 1) % 2 == 0) {
                     for (int i = 0; i < board[row].length; i += 2) {
-                        board[row][i] = new Pawn("black", row, i);
-                        blackPieces--;
+                        if (blackPieces > 0) {
+                            board[row][i] = new Pawn("black", row, i);
+                            blackPieces--;
+                        }
                     }
                 } else {
                     for (int i = 1; i < board[row].length; i += 2) {
-                        board[row][i] = new Pawn("black", row, i);
-                        blackPieces--;
+                        if (blackPieces > 0) {
+                            board[row][i] = new Pawn("black", row, i);
+                            blackPieces--;
+                        }
                     }
                 }
-            }
         }
         for (int row = board.length - 1; row >= 0; row--) {
-            if (whitePieces >= 0) {
                 if ((row - 1) % 2 == 0) {
-                    for (int i = board[row].length-1; i > 0; i--) {
-                        board[row][i] = new Pawn("white", row, i);
-                        whitePieces--;
+                    for (int i = 0; i < board[row].length; i += 2) {
+                        if (whitePieces > 0) {
+                            board[row][i] = new Pawn("white", row, i);
+                            whitePieces--;
+                        }
                     }
                 } else {
-                    for (int i = board[row].length-2; i > 0; i--) {
-                        board[row][i] = new Pawn("white", row, i);
-                        whitePieces--;
+                    for (int i = 1; i < board[row].length; i += 2) {
+                        if (whitePieces > 0) {
+                            board[row][i] = new Pawn("white", row, i);
+                            whitePieces--;
+                        }
                     }
-                }
             }
         }
         return board;
