05ee6a0f5f839f394d1889e082bae1af6a3865c1
gabriellaszigeti <gbszgt@gmail.com>
2021-06-08 12:08:31 +0200

    Feat: Create skeleton of constructor in pawn class

--- src/main/java/com/branchhunters/polishdraughts/Pawn.java
+++ src/main/java/com/branchhunters/polishdraughts/Pawn.java
@@ -1,4 +1,13 @@
 package com.branchhunters.polishdraughts;
+
 // pawn
 public class Pawn {
+    private String color;
+    private boolean isCrowned;
+    private Coordinates coordinates;
+
+    public Pawn(String color, boolean isCrowned, Coordinates coordinates) {
+
+
+    }
 }
