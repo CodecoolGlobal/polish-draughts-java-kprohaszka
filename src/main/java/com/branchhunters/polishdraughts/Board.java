2c2b7cd7fadc8971ead76f73241d38e3cf93928c
Sairieth <henrik.riki.marcz@gmail.com>
2021-06-08 12:08:29 +0200

    feat:  started implementing board constructor

--- src/main/java/com/branchhunters/polishdraughts/Board.java
+++ src/main/java/com/branchhunters/polishdraughts/Board.java
@@ -1,4 +1,16 @@
 package com.branchhunters.polishdraughts;
 
 public class Board {
+ private Pawn[][] board;
+
+    Board(int n){
+    board = new Pawn[n][n];
+    Pawn black = new Pawn(black,false,coordinates(x,y));
+    Pawn white = new Pawn(white,false,coordinates(x,y));
+    for (Pawn[] row: board){
+        for (int i = 0; i < row.length; i++) {
+
+        }
+    }
+    }
 }
