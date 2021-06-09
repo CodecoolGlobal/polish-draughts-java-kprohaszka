8ff37d81966d874e9343832604efbfa1b4763287
azarath1 <ar2oww@gmail.com>
2021-06-09 18:04:20 +0200

    add: prompt for start game

--- src/main/java/com/branchhunters/polishdraughts/Game.java
+++ src/main/java/com/branchhunters/polishdraughts/Game.java
@@ -1,5 +1,6 @@
 package com.branchhunters.polishdraughts;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Game {
@@ -7,18 +8,21 @@ public class Game {
     public Game() throws InterruptedException {
         menu();
     }
-
     public void menu() throws InterruptedException {
         System.out.println("POLISH DRAUGHTS");
         TimeUnit.SECONDS.sleep(2);
+        promptEnterKey();
     }
-
+    public void promptEnterKey(){
+        System.out.println("Hit \"ENTER\" to Start");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+    }
     public void start() {
         Board board = new Board();
         System.out.println("White moves first...");
         Board.printBoard(board.getBoard());
     }
-
     public static void main(String[] args) throws InterruptedException {
         Game game = new Game();
         game.start();
