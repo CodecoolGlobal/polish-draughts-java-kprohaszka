e109d1a5467d1772c5cf817ca8f8d1ebea6ede14
azarath1 <ar2oww@gmail.com>
2021-06-09 18:02:24 +0200

    add: delay after title for smooth render

--- src/main/java/com/branchhunters/polishdraughts/Game.java
+++ src/main/java/com/branchhunters/polishdraughts/Game.java
@@ -1,13 +1,16 @@
 package com.branchhunters.polishdraughts;
 
+import java.util.concurrent.TimeUnit;
+
 public class Game {
 
-    public Game() {
+    public Game() throws InterruptedException {
         menu();
     }
 
-    public void menu() {
+    public void menu() throws InterruptedException {
         System.out.println("POLISH DRAUGHTS");
+        TimeUnit.SECONDS.sleep(2);
     }
 
     public void start() {
@@ -16,7 +19,7 @@ public class Game {
         Board.printBoard(board.getBoard());
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Game game = new Game();
         game.start();
     }
