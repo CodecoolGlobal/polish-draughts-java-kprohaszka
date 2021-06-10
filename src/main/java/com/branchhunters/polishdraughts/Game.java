5df9a255003feea9c499564e3b01f02dc3fa457d
azarath1 <ar2oww@gmail.com>
2021-06-10 08:12:58 +0200

    feat: playRound(); start(); getBoard(); methods TODO: playRound() method logics

--- src/main/java/com/branchhunters/polishdraughts/Game.java
+++ src/main/java/com/branchhunters/polishdraughts/Game.java
@@ -1,34 +1,59 @@
 package com.branchhunters.polishdraughts;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class Game {
+    private Board board;
+    private boolean gameIsRunning;
 
     public Game() throws InterruptedException {
         menu();
     }
+
+    public Board getBoard() {
+        return this.board;
+    }
+
     public void menu() throws InterruptedException {
         System.out.println("POLISH DRAUGHTS");
         TimeUnit.SECONDS.sleep(2);
         promptEnterKey();
     }
-    public void promptEnterKey(){
+
+    public void promptEnterKey() {
         System.out.println("Hit \"ENTER\" to Start");
         Scanner scanner = new Scanner(System.in);
         scanner.nextLine();
         clearScreen();
     }
+
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+
     public void start() {
-        Board board = new Board();
+        this.board = new Board();
         clearScreen();
         System.out.println("White moves first...");
         Board.printBoard(board.getBoard());
+        this.gameIsRunning = true;
+        while (gameIsRunning) {
+            playRound();
+        }
     }
+
+    private void playRound() {
+        clearScreen();
+        Board.printBoard(board.getBoard());
+        promptEnterKey();
+
+    }
+
+
     public static void main(String[] args) throws InterruptedException {
         Game game = new Game();
         game.start();
